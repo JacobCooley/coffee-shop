7ccd399123d3e73be142643063cd6cb290cf9d46
Jacob Cooley <j.cooley42@gmail.com>
2018-12-23 20:25:34 +0100

    Trying to setup ssh

--- src/modules/home/HomeComponent.jsx
+++ src/modules/home/HomeComponent.jsx
@@ -5,7 +5,7 @@ import './Home.scss'
 const HomeComponent = () => (
 	<div className='home'>
 		<h3>Token Launcher Alpha</h3>
-		<div>Make some contracts</div>
+		<div>Make some contracts.</div>
 	</div>
 )
 
