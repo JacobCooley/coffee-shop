15345042e5eff770d8408d7ddcbb3096e4a5dd0d
Jacob Cooley <j.cooley42@gmail.com>
2018-12-24 14:04:48 +0100

    Updated chain radio

--- src/common/components/Input/Input.jsx
+++ src/common/components/Input/Input.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import './Input.scss'
 
 const InputComponent = ({ label, type, name, onChange, defaultValue, value }) => (
-	<>
+	<div className='input'>
 		<label id={name}>
 			{label} :
 			<input value={value} defaultValue={defaultValue} onChange={onChange} type='text' name={name} />
 		</label>
-	</>
+	</div>
 )
 
 export default InputComponent;
