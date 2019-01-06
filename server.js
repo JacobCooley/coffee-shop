ef20ff10dcfb6beec6c7966555da369c6336237a
Jacob Cooley <j.cooley42@gmail.com>
2019-01-06 23:23:27 +0100

    New forcehttps function

--- server.js
+++ server.js
@@ -1,23 +1,16 @@
 const express = require('express')
 const fs = require('fs')
 const https = require('https')
-const privateKey = fs.readFileSync('../ganjafarm/certs/server.key', 'utf8')
-const certificate = fs.readFileSync('../ganjafarm/certs/server.cert', 'utf8')
-const credentials = { key: privateKey, cert: certificate }
+// const privateKey = fs.readFileSync('../ganjafarm/certs/server.key', 'utf8')
+// const certificate = fs.readFileSync('../ganjafarm/certs/server.cert', 'utf8')
+// const credentials = { key: privateKey, cert: certificate }
 const path = require('path')
 const port = 4269
 const app = express()
-app.enable('trust proxy')
+// app.enable('trust proxy')
 app.use(express.static(__dirname))
-app.use(express.static(path.join(__dirname, 'dist')))
+// app.use(express.static(path.join(__dirname, 'dist')))
 
-app.use(function(req,res,next) {
-	if (!/https/.test(req.protocol)){
-		res.redirect("https://" + req.headers.host + req.url);
-	} else {
-		return next();
-	}
-});
 app.use (function (req, res, next) {
 	if (req.secure) {
 		// request was via https, so do no special handling
@@ -30,9 +23,10 @@ app.use (function (req, res, next) {
 	}
 });
 
-app.get('/*', function (req, res) {
+app.get('*', function (req, res) {
+	console.log('sent')
 	res.sendFile(path.join(__dirname, 'dist/index.html'))
 })
-const server = https.createServer(credentials, app)
+// const server = https.createServer(credentials, app)
 
-server.listen(port)
+app.listen(port)
