23fd988269136adbf655be6b7f63d688352e9057
Jacob Cooley <j.cooley42@gmail.com>
2019-01-06 23:11:04 +0100

    New forcehttps function

--- server.js
+++ server.js
@@ -11,11 +11,20 @@ app.enable('trust proxy')
 app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname, 'dist')))
 
+app.use(function(req,res,next) {
+	if (!/https/.test(req.protocol)){
+		res.redirect("https://" + req.headers.host + req.url);
+	} else {
+		return next();
+	}
+});
 app.use (function (req, res, next) {
 	if (req.secure) {
 		// request was via https, so do no special handling
+		console.log('https')
 		next();
 	} else {
+		console.log('http')
 		// request was via http, so redirect to https
 		res.redirect('https://' + req.headers.host + req.url);
 	}
