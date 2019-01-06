c01b319962e8aea2be77112c7d138ad8699bed76
Jacob Cooley <j.cooley42@gmail.com>
2019-01-06 18:53:07 +0100

    Updated for https

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 const express = require('express');
 const favicon = require('express-favicon');
+const fs = require('fs');
+const https = require('https');
+const privateKey  = fs.readFileSync('../ganjafarm/certs/server.key', 'utf8');
+const certificate = fs.readFileSync('../ganjafarm/certs/server.crt', 'utf8');
+const credentials = {key: privateKey, cert: certificate};
 const path = require('path');
 const port = 4269;
 const app = express();
 // app.use(favicon(__dirname + '/dist/favicon.ico'));
 
-//FORCE HTTP UNTIL WE GET THE SERVER HOOKED TO HTTPS
-// app.use(function (req, res, next){
-// 	if (req.headers['x-forwarded-proto'] === 'https') {
-// 		res.redirect('http://' + req.hostname + req.url);
-// 	} else {
-// 		next();
-// 	}
-// });
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/*', function (req, res) {
 	res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
-app.listen(port);
+const server = https.createServer(credentials, app);
+
+server.listen(port);
