b1c72993f55e7bc4ecf607c054e82174248f6562
Jacob Cooley <j.cooley42@gmail.com>
2019-01-15 16:59:44 +0100

    Added untracked files

---
+++ src/modules/app/components/404/index.js
@@ -0,0 +1,2 @@
+import four04 from './404'
+export default four04
---
+++ src/modules/app/components/footer/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { FaFacebookSquare, FaTwitterSquare, FaTelegramPlane, FaLinkedin, FaSlack, FaRedditSquare } from 'react-icons/fa';
+import './Footer.scss'
+
+const Footer = () => {
+	return (
+		<footer>
+			<div className='social-media'>
+				<a><FaFacebookSquare /></a>
+				<a><FaTwitterSquare /></a>
+				<a><FaTelegramPlane /></a>
+				<a><FaSlack /></a>
+				<a><FaRedditSquare /></a>
+				<a target='_blank' href='https://www.linkedin.com/in/jacob-cooley-03a1396b/'><FaLinkedin /></a>
+			</div>
+			<div className='site-info'>
+				<a href="mailto:j.cooley42@gmail.com?Subject=Greetings!" target="_top">Contact</a>
+				<div>Token Launcher &copy; 2019</div>
+			</div>
+		</footer>
+	)
+}
+
+export default Footer
